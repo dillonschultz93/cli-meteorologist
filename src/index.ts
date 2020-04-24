273bad270664d1eced4f70d9b768afda187f8009
Dillon Schultz <dillonschultz93@gmail.com>
2020-04-23 20:48:04 -0700

    Initial setup

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+const program = require('commander');
+
+program
+	.version('1.0.0')
+	.description('A CLI tool that returns various weather forecasts');
+
+program
+	.command('today')
+	.alias('t')
+	.description('Show the weather information for today')
+	.action(() => {
+		console.log('Today is a nice day');
+	});
+
+program.parse(process.argv);
